7d96106784b795e720396b91622c927707141d99
linux <ilikemyemailsalottbh@proton.me>
2024-04-28 09:39:34 -0400

    Got some stuff on scaffold done.

--- src/main/java/com/epicalyx/addon/modules/EpicalyxScaffold.java
+++ src/main/java/com/epicalyx/addon/modules/EpicalyxScaffold.java
@@ -4,6 +4,7 @@ import meteordevelopment.meteorclient.events.entity.player.ClipAtLedgeEvent;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
+import net.minecraft.entity.player.PlayerEntity;
 
 public class EpicalyxScaffold extends Module {
     public EpicalyxScaffold() {
@@ -12,6 +13,10 @@ public class EpicalyxScaffold extends Module {
 
     @EventHandler
     private void onClipAtLedge(ClipAtLedgeEvent event) {
-        if (!mc.player.isSneaking()) event.setClip(true);
+        if (!mc.player.isSneaking()) {
+            event.setClip(true);
+            float pitch = 82f;
+            ((PlayerEntity)mc.player).setPitch(pitch);
+        }
     }
 }
